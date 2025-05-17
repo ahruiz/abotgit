224e4a311e0088c1c0ae5f7887f2da7f80d82078
ahruiz <ahruiz0@gmail.com>
2025-05-17 16:15:42 -0700

    codigo para contar letras de una palabra o texto

---
+++ cuentaLetras.py
@@ -0,0 +1,11 @@
+#codigo para contar las letras de una palabra
+print("-" * 80)
+print("Vamos a contar las letras de una o varias palabras dadas por el usuario")
+
+texto = input("Introduce un texto o una palabra: ")
+txtsinespacio = texto.replace(" ","") #eliminamos los espacios
+
+longPal = len(txtsinespacio)
+
+print(f"El texto es: [{texto}] y tiene una longitud de {longPal} ")
+print("-" * 80)
